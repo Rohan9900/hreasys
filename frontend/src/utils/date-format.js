837fde94b7ee244fa173f9997767139f896b092d
Rohan9900 <62924337+Rohan9900@users.noreply.github.com>
2022-01-10 18:31:22 +0530

    Delete frontend directory

--- frontend/src/utils/date-format.js
+++
@@ -1,8 +0,0 @@
-export default function formatDate(date) {
-    const d = new Date(date);
-    const month = (d.getMonth() + 1).toString();
-    const day = d.getDate().toString();
-    const year = d.getFullYear();
-
-    return [year, month, day].join('-');
-}
